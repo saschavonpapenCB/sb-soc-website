d8bc00fba4c28480eb41ea08c8cc2f0d3579f027
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-04-25 00:02:10 +1000

    changed to port 3000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
